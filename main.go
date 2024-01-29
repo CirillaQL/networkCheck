367aecd69061e6726cda4a7fd89801c2505ee4fe
Qian Lu <qian.lu@nets-tech.com>
2024-01-29 13:57:04 +0800

    update comment

--- main.go
+++ main.go
@@ -189,7 +189,7 @@ func FindPodNetworkMonitor(namespace, pod string) bool {
 		}
 		return true
 	default:
-		fmt.Println("Wrong data format. Can't handle this type of prometheus response.")
+		fmt.Println("Wrong data format. Can't handle this type of prometheus response. Please check result of prometheus")
 		return false
 	}
 }
